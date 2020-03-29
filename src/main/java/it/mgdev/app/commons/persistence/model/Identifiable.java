61582176c5d849bfe5da79bfc6ea2097f8ec5eaf
milazzo.ga@gmail.com <milazzo.ga@gmail.com>
2020-03-29 10:53:10 +0200

    +version

--- src/main/java/it/mgdev/app/commons/persistence/model/Identifiable.java
+++ src/main/java/it/mgdev/app/commons/persistence/model/Identifiable.java
@@ -3,6 +3,8 @@ package it.mgdev.app.commons.persistence.model;
 public interface Identifiable {
 
     String id = "id";
+    String version = "version";
 
     Long getId();
+    int getVersion();
 }
